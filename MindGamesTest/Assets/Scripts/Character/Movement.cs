b1ed4481c971873e3c6df4ab718a92fac3d46e70
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2024-06-21 23:03:48 +0300

    Add simple correct movement sync

---
+++ MindGamesTest/Assets/Scripts/Character/Movement.cs
@@ -0,0 +1,62 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Character
+{
+    [AddComponentMenu("Scripts/Character/Character.Movement")]
+    internal class Movement : NetworkBehaviour
+    {
+        [SerializeField]
+        private InputAction movementInput;
+
+        [SerializeField]
+        private float speed;
+
+        [SerializeField]
+        private bool isReadInput = false;
+
+        [SerializeField]
+        private NetworkVariable<Vector3> netPosition = new();
+
+        public override void OnNetworkSpawn()
+        {
+            if ((IsServer || !IsOwner) && !IsHost)
+            {
+                return;
+            }
+
+            movementInput.Enable();
+            movementInput.performed += MovementInputPerformed;
+            movementInput.canceled += MovementInputCanceled;
+        }
+
+        private void MovementInputCanceled(InputAction.CallbackContext context)
+        {
+            isReadInput = false;
+        }
+
+        private void MovementInputPerformed(InputAction.CallbackContext context)
+        {
+            isReadInput = true;
+        }
+
+        private void Update()
+        {
+            if (isReadInput)
+            {
+                Vector2 direction = movementInput.ReadValue<Vector2>();
+                Vector3 moveDelta = speed * Time.deltaTime * (Vector3)direction;
+                MoveServerRpc(moveDelta);
+            }
+
+            transform.position = netPosition.Value;
+        }
+
+        [ServerRpc]
+        private void MoveServerRpc(Vector3 moveDelta)
+        {
+            netPosition.Value += moveDelta;
+        }
+    }
+}
---
+++ MindGamesTest/Assets/Scripts/Common/NetworkManagerHud.cs
@@ -0,0 +1,59 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace Common
+{
+    [AddComponentMenu("Scripts/Common/Common.NetworkManagerHud")]
+    internal class NetworkManagerHud : MonoBehaviour
+    {
+        [SerializeField]
+        private NetworkManager networkManager;
+
+        private void OnGUI()
+        {
+            GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+            if (!networkManager.IsClient && !networkManager.IsServer)
+            {
+                StartButtons();
+            }
+            else
+            {
+                StatusLabels();
+            }
+
+            GUILayout.EndArea();
+        }
+
+        private void StartButtons()
+        {
+            if (GUILayout.Button("Host"))
+            {
+                _ = networkManager.StartHost();
+            }
+
+            if (GUILayout.Button("Client"))
+            {
+                _ = networkManager.StartClient();
+            }
+
+            if (GUILayout.Button("Server"))
+            {
+                _ = networkManager.StartServer();
+            }
+        }
+
+        private void StatusLabels()
+        {
+            string mode = networkManager.IsHost
+                ? "Host"
+                : networkManager.IsServer
+                    ? "Server"
+                    : "Client";
+
+            GUILayout.Label(
+                "Transport: " + networkManager.NetworkConfig.NetworkTransport.GetType().Name
+            );
+            GUILayout.Label("Mode: " + mode);
+        }
+    }
+}
